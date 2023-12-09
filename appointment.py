5ff8c6c67f02f1b45da310239749e21071f40158
grantly2031 <73861368+grantly2031@users.noreply.github.com>
2023-12-08 18:05:13 -0700

    Add files via upload

--- appointment.py
+++ appointment.py
@@ -1,39 +1,78 @@
-class Appointment:
-    
-    #Constructor:
-    def __init__(self, day_of_week, start_time_hour):
-        self.client_name = ""   
-        self.client_phone = ""  
-        self.appt_type = 0      
-        self.day_of_week = day_of_week
+
+'''
+
+# PART 1
+
+Class Name: appointment
+
+Attributes (Hidden): client_name                    string
+                    client_phone                    string
+                    appt_type (appointment type)    int
+                    day_of_the_week                 string
+                    start_time_hour                 string
+
+Methods:
+    Getter for every Attribute
+    Setter for client_name, client_phone and appt_type
+    appt_type_description
+    end_time_hour
+    schedule
+    format_record
+    string printing method
+
+'''
+
+
+class appointment:
+
+
+    '''
+    Constructor to initialize the variables
+    It is made parameterized (As per requirements)
+    '''
+
+    def __init__(self, day_of_the_week, start_time_hour):
+        self.client_name = ""
+        self.client_phone = ""
+        self.appt_type = 0
+        self.day_of_the_week = day_of_the_week
         self.start_time_hour = start_time_hour
 
-    #Getters:
+
+# GETTERS
+
     def get_client_name(self):
         return self.client_name
-    
+
     def get_client_phone(self):
         return self.client_phone
-    
+
     def get_appt_type(self):
         return self.appt_type
-    
+
     def get_day_of_week(self):
-        return self.day_of_week
-    
+        return self.day_of_the_week
+
     def get_start_time_hour(self):
         return self.start_time_hour
-    
-    #additional “getter” method that translates and returns the object’s appt_type as a text description
+
+
     def get_appt_type_desc(self):
-        appt_types = {0: "Available", 1: "Mens Cut", 2: "Ladies Cut", 3: "Mens Colouring", 4: "Ladies Colouring"}
-        return appt_types.get(self.appt_type, "Sorry that is not a valid appointment type!")
+        appt_type_dictionary = {
+            0: "Available",
+            1: "Mens Cut",
+            2: "Ladies Cut",
+            3: "Mens Colouring",
+            4: "Ladies Colouring"
+        }
+        return appt_type_dictionary.get(self.appt_type, "Appointment Number is wrong")
 
-    #additional “getter” method that returns the value of the object’s start_time_hour + 1
+
     def get_end_time_hour(self):
-        return self.start_time_hour + 1
+        return int(self.start_time_hour) + 1
 
-    # Setters:
+
+# SETTERS
     def set_client_name(self, client_name):
         self.client_name = client_name
 
@@ -43,24 +82,22 @@ class Appointment:
     def set_appt_type(self, appt_type):
         self.appt_type = appt_type
 
-    #schedule appointment
+
     def schedule(self, client_name, client_phone, appt_type):
-        self.set_client_name(client_name)
-        self.set_client_phone(client_phone)
-        self.set_appt_type(appt_type)
+        self.client_name = client_name
+        self.client_phone = client_phone
+        self.appt_type = appt_type
 
-    #cancel appointment
+
     def cancel(self):
-        self.set_client_name("")
-        self.set_client_phone("")
-        self.set_appt_type(0)
+        self.client_name = ""
+        self.client_phone = ""
+        self.appt_type = 0
 
-    #return CSV string representation
+
     def format_record(self):
-        return f"{self.client_name},{self.client_phone},{self.appt_type},{self.day_of_week},{self.start_time_hour}"
+        return f"{self.client_name},{self.client_phone},{self.appt_type},{self.day_of_the_week},{self.start_time_hour:02}"
 
-    #return string representation of the appointment object
+
     def __str__(self):
-        start_time_str = f"{self.start_time_hour:02d}:00"
-        end_time_str = f"{self.get_end_time_hour():02d}:00"
-        return f"{self.client_name:19s} {self.client_phone:14s} {self.day_of_week:9s} {start_time_str:7s} - {end_time_str:9s} {self.get_appt_type_desc():19s}"
+        return f"{self.client_name} {self.client_phone} {self.day_of_the_week} {self.start_time_hour:02}:00 - {self.get_end_time_hour():02}:00 {self.get_appt_type_desc()}"
--- appt_manager.py
+++ appt_manager.py
@@ -1,96 +1,213 @@
-import appointment
 
-weekly_calendar = []
+'''
 
-def create_weekly_calendar(): #Nam
-    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    for day in days_of_week:
-        for hour in range(9, 17): #start_time_hour: 0900 to 1600
-            weekly_calendar.append(appointment.Appointment(day, hour)) #pass "day" to "day_of_week" and pass "hour" to "start_time_hour"
+# PART 2
 
-def load_scheduled_appointments(): #Gordon
-    # ???
-    # ???
-    # ???
-    # print("File not found.")
-    pass
+File Name: appt_manager
 
-def print_menu():
-    print("Jojo's Hair Salon Appointment Manager")
-    print("=====================================")
-    print("1) Schedule an appointment")
-    print("2) Find appointment by name")
-    print("3) Print calendar for a specific day")
-    print("4) Cancel an appointment")
-    print("9) Exit the system")
-    selection = input("Enter your selection: ")
-    return selection
+'''
+
+import appointment as apt
+import csv
+from pathlib import Path
 
 
-def find_appointment_by_time(): #Nam
-    pass
+'''
+Function Name: create_weekly_calender
+Parameters: none
+Returns: list of appointments
+Description: Creates a list of appointments. This list serves as a template for the appointments for Days (Mon - Sat)
+             and the hours from 9 to 16  (24) Hour Format.
+             The calender is used for saving appointments which are read from csv file. 
+
+'''
 
 
-def show_appointments_by_name(client_name): #Sam
-    for appt in weekly_calendar:
-        if client_name.lower() in appt.get_client_name().lower():
-            print(appt)
+def create_weekly_calender():
+    calendar = []
+    days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    available_hours = ["09", "10", "11", "12", "13", "14", "15", "16"]
+
+    for day in days_of_the_week:
+        for hour in available_hours:
+            obj = apt.appointment(day, hour)
+            calendar.append(obj)
+
+    return calendar
+
+'''
+Function Name: print_menu
+Parameters: calender
+Returns: none
+Description: The function prints menu to prompt the user to select any option.
+'''
 
 
-def show_appointments_by_day(day): #Sam
-    for appt in weekly_calendar:
-        if appt.get_day_of_week().lower() == day.lower():
-            print(appt)
+def print_menu(calender):
+
+    while True:
+
+        print("Jojo\'s Hair Salon Appointment Manager")
+        print("======================================")
+        print("1) Schedule an appointment")
+        print("2) Find appointment by name")
+        print("3) Print calendar for a specific day")
+        print("4) Cancel an appointment")
+        print("9) Exit the system")
+        choice = int(input("Enter Your Selection: "))
+
+        if 1 <= choice <= 4 or choice == 9:
+
+            if choice == 1:
+                schedule()
+
+            elif choice == 2:
+                show_appointments_by_name()
+
+            elif choice == 3:
+                print_calender(calender)
+
+            elif choice == 4:
+                cancel_appointment(calender)
+
+            elif choice == 9:
+                return None
+
+        else:
+            print("Invalid choice. Please enter a number between 1 (Inclusive) and 4 (Inclusive) or 9 to exit.")
+            choice = int(input("Enter Your Selection: "))
 
 
+'''
+Function Name: cancel_appointment
+Parameters: calender
+Returns: None
+Description: Cancels any appointment. Prompts the user to enter day and time. Appointment is searched via find_appointment_by_time function
+             When the appointment is found, the cancel function of the appointment class is invoked 
+'''
 
-def save_scheduled_appointments(): #Gordon
-    # print("\n** Schedule an appointment **")
-    # ??? = input("What day: ")
-    # ??? = int(input("Enter start hour (24 hour clock): "))
-    # ??? = input("Client Name: ")
-    # ??? = input("Client Phone: ")
-    # print("Appointment types\n1: Mens Cut $50, 2: Ladies Cut $80, 3: Mens Colouring $50, 4: Ladies Colouring $120")
-    # ??? = int(input("Type of Appointment: "))
 
-    # print("Sorry, that is not a valid appointment type!")
-    # print("Sorry, that time slot is not available!")
+def cancel_appointment(calender):
 
-    # print("OK, ??? appointment is scheduled!")
-    pass
+    print("** Cancel an appointment **")
+    day = input("What day: ")
+    hour = input("Enter start hour (24 hour clock): ")
+    appt = find_appointment_by_time(calender, day, hour)
 
-def cancel_appointment(): #?????
-    pass
+    if appt:
+        print(f"{day}, {hour}, - , {appt.get_end_time_hour()}, for , {appt.get_client_name()}, has been cancelled!")
+        appt.cancel()
 
-def main(): #Gordon
-    create_weekly_calendar()
-    print("Starting the Appointment Manager System")
-    print("Weekly calendar created")
-    # ??? = input("Would you like to load previously scheduled appointments from a file (Y/N)? ").lower()
-        # ??? = input("Enter appointment filename: ")
-        # ??? = print("File not found.")
-        # ??? = input("Re-enter appointment filename: ")
-        # ??? = print("???? previously scheduled appointments have been loaded")
+    else:
+        print("Appointment Not Found")
+
+
+'''
+
+Function Name: find_appointment_by_time
+Parameters: Calender, day, time
+Returns: A single appointment or none
+Description: Finds a single appointment in the calender via searching by day and time
+
+'''
+
+
+def find_appointment_by_time(calender, day, start_hour):
+    for appointment in calender:
+        if appointment.get_day_of_week() == day and appointment.get_start_time_hour() == start_hour:
+            return appointment
+
+    return None
+
+
+'''
+
+Function Name: load_scheduled_appointments
+Parameters: calender
+Returns: count(integer) of how many appointments have been loaded
+Description: Reads the csv file and loads the appointments in the calender
+
+'''
+
+
+def load_scheduled_appointments(calender):
+
+    while True:
+        file_choice = input("Would you like to load previously scheduled appointments from a file (Y/N)? ").upper()
+
+        if file_choice == 'Y':
+            file_name = input("Enter Appointment filename: ")
+            path = Path(file_name)
+
+            if path.exists():
+                break
+            else:
+                print("File Not Found, Re-enter the appointment filename: ")
+        elif file_choice == 'N':
+            print("Not Loading From a File (New Appointments Will be created)")
+            return 0
+        else:
+            print("Invalid choice. Please enter 'Y' or 'N'.")
+
+    counter = 0
+    with open(file_name, 'r') as file:
+        read_obj = csv.reader(file)
+
+        for row in read_obj:
+            counter += 1
+
+    '''
     
-    userSelection = print_menu()    # pass selection into userSelection
-    if userSelection == "1":
-        save_scheduled_appointments()
-    elif userSelection == '2':
-        client_name = input("Enter Client Name: ")
-        show_appointments_by_name(client_name)
-    elif userSelection == '3':
-        day = input("Enter day of week: ")
-        show_appointments_by_day(day)
-    elif userSelection == '4':
-        cancel_appointment()    
-    elif userSelection == '9':
-        input("Would you like to save all scheduled appointments to a file (Y/N)? ").lower()
-        # ??? = input("Enter appointment filename: ")
-        # ???
-        # ???
-        print("Good Bye!")
-        
+    PLEASE WRITE THE CODE TO READ THE CSV FILE AND SCHEDULE APPOINTMENTS IN THE CALENDER
+    
+    '''
+    return counter
+
+
+def show_appointments_by_name():
+    print("Implementation Remaining...")
+
+
+def schedule():
+    print("Implementation Remaining...")
+
+
+'''
+Function Name: print_calender
+Parameters: Calender
+Returns: None
+Description: Prints the appointment calender
+
+'''
+
+
+def print_calender(calender):
+    day = input("Enter day of week: ")
+    print(f"Appointments for {day}")
+
+    print(f"Client Name".ljust(20), "Phone".ljust(20), "Date".ljust(20), "Start".ljust(20), "End".ljust(20), "Type")
+    print("-".ljust(120, "-"))
+
+    for appointments in calender:
+        if appointments.get_day_of_week() == day:
+            print(appointments.get_client_name().ljust(20), appointments.get_client_phone().ljust(20), appointments.get_end_time_hour(), "          ", appointments.get_appt_type())
+
+    return
+
+
+def main():
+    print("Starting the Appointment Manager System")
+
+    calender = create_weekly_calender()
+
+    print("Weekly Calender Created")
+
+    counter = load_scheduled_appointments(calender)
+    print(f"{counter}  previously scheduled appointments have been loaded")
+
+    if counter != 0:
+        print_menu(calender)
+
 
 if __name__ == "__main__":
-    main()
-
+    main();
